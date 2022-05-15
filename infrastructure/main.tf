70547279d328113b0ae6f69b2646f094a61d2992
caiofelixreis <cfelix@seidor.es>
2022-05-15 17:10:21 +0100

    first terraform test working

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,29 +1,28 @@
 resource "aws_s3_bucket" "datalake" {
-    bucket= var.datalake_bucket_name
-    acl = "private"
+  bucket = var.datalake_bucket_name
+  acl    = "private"
 
-    server_side_encryption_configuration {
-      rule {
-          apply_server_side_encryption_by_default {
-              sse_algotithm = "AE256"
-          }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
       }
     }
-
-    tags = {
-        PURPOUSE = "learning"
-    }
+  }
+  tags = {
+    PURPOUSE = "learning"
+  }
 }
 
 resource "aws_s3_bucket_object" "python_code" {
-  bucket = aws_s3_bucket.datalake
-  key = "code/python/job_tf.py"
-  acl="private"
-  source="../job.py"
-  etag= filemd5("../job.py")
+  bucket = aws_s3_bucket.datalake.id
+  key    = "code/python/job_tf.py"
+  acl    = "private"
+  source = "../job.py"
+  etag   = filemd5("../job.py")
 }
 
 provider "aws" {
-    profile = "default"
-    region = var.aws_default_region
+  profile = "default"
+  region  = var.aws_default_region
 }
